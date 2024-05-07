d5fd1f7d9bf1878ecc88d3b6243a0a7691b39173
Robertarezza <roberta.rezza@gmail.com>
2024-05-07 15:45:32 +0200

    add: components AppSarch, file store.js

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script>
 import axios from "axios";
+import { store } from "./store";
 import AppCards from "./components/AppCards.vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppLoad from "./components/AppLoad.vue";
+import AppSearch from "./components/AppSearch.vue";
+
 
 export default {
   components: {
     AppHeader,
     AppCards,
     AppLoad,
+    AppSearch,
   },
 
   data() {
     return {
       cardsArray: [],
       isLoading: false,
+      store,
+    
     };
   },
 
@@ -33,11 +39,36 @@ export default {
       
     });
   },
+  methods: {
+    getCards() {
+      this.isLoading = true;
+      // Costuriamo i parametri per la chiamata axios
+       const paramsObj = {
+       
+       };
+
+      // Se c'è il filtro applicato, aggiungo anche chiave status all'oggetto di params
+      if (this.store.selectedStatus !== "All") {
+        paramsObj.status = this.store.selectedStatus;
+         console.log("Get cards", this.store.selectedStatus)
+      }
+
+      axios
+        .get("https://rickandmortyapi.com/api/character", {
+          params: paramsObj,
+        })
+        .then((resp) => {
+          this.cardsArray = resp.data.results;
+          this.isLoading = false;
+        });
+    },
+  },
 };
 </script>
 
 <template>
   <AppHeader />
+  <AppSearch  @filter="getCards"/>
   <AppLoad v-if="isLoading" />
   <AppCards v-else :cardsArray="cardsArray" />
 </template>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,30 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+      status: ["All", "unknown", "Alive", "Dead"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-content-center flex-wrap-wrap">
+    <h5>Seleziona i personaggi in base allo STATUS</h5>
+    <select @change="$emit('filter')" v-model="store.selectedStatus">
+      <option :value="statu" v-for="statu in status">
+        {{ statu }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+select {
+  width: 10%;
+  margin: 0 auto;
+}
+</style>
